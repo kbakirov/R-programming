d2ecdc0b501238582cb05751fde62e4095e8ee52
kbakirov <bakir.kuanysh@gmail.com>
2020-01-31 18:34:59 +0100

    commit

--- Decision_Tree/DecisionTree.R
+++ Decision_Tree/DecisionTree.R
@@ -1,1 +1,7 @@
 library(rpart)
+#setting working directory
+print(getwd())
+setwd("C:/Users/Kuanysh/Documents/GitHub/R-programming/Decision_Tree")
+
+#reading StudentData
+data <- read.csv("StudentData.csv")
